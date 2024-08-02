ffd0c1284438b45353d1cdba9129679bc011379e
sanas12 <162470494+sanas12@users.noreply.github.com>
2024-08-02 09:30:10 +0200

    Fixing errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  imports: [RouterModule], // Import RouterModule here
 })
 export class AppComponent {
   title = 'myFlix-Angular-client';
